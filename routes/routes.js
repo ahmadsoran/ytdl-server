f7b2f8bcc3928a70977e7fc89695fbb4ab0dd0a1
ahmadsoran <ahmadsorann@gmail.com>
2022-04-07 20:31:51 +0300

    .

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,12 @@ import { Router } from "express";
 const Route = Router();
 import VideoRoutes from './controllers/video/video-download.js';
 
+Route.options('/download', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.sendStatus(200);
+})
 Route.post("/download", VideoRoutes.DownloadVideo)
 Route.post("/getVideoInfo", VideoRoutes.getVideoInfo)
 
